31c2e0be3571911df0f9d401378c6e8090ecf260
kostashassouros <kostas.hassouros@gmail.com>
2022-01-27 19:11:20 +1030

    First Commit

--- dist/cjs/types/components/MarkedText/MarkedText.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-export interface MarkTextProps {
-    textToMark: string;
-    isCaseSensitive: boolean;
-    markedWords: Array<string>;
-    markerColor: string;
-}
-declare const MarkedText: ({ textToMark, isCaseSensitive, markedWords, markerColor }: MarkTextProps) => JSX.Element;
-export default MarkedText;
